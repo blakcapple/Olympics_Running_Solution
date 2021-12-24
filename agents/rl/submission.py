d7baa0724a23c5a5c8aa238803965482604307d4
ruanyudi <724533614@qq.com>
2021-12-24 22:33:40 +0800

    update submission.py

--- agents/rl/submission.py
+++ agents/rl/submission.py
@@ -20,6 +20,53 @@ class Flatten(nn.Module):
     def forward(self, x):
         return x.view(x.size(0), -1)
 
+class CNNLayer2(nn.Module):
+    
+    out_channel = 32
+    hidden_size = 64
+    kernel_size = 3
+    stride = 1
+    use_Relu = True
+    use_orthogonal = True
+    
+    def __init__(self, state_shape):
+        
+        super().__init__()
+        
+        active_func = [nn.Tanh(), nn.ReLU()][self.use_Relu]
+        init_method = [nn.init.xavier_uniform_, nn.init.orthogonal_][self.use_orthogonal]
+        gain = nn.init.calculate_gain(['tanh', 'relu'][self.use_Relu])
+        input_channel = state_shape[0]
+        input_width = state_shape[1]
+        input_height = state_shape[2]
+        def init_(m):
+            return init(m, init_method, lambda x: nn.init.constant_(x, 0), gain=gain)
+        cnn1_out_shape = [input_width - self.kernel_size + self.stride, input_height - self.kernel_size + self.stride]
+        pool_out_shape = [int((cnn1_out_shape[0] - 2)/2) + 1, int((cnn1_out_shape[0] - 2)/2) + 1 ]
+        cnn2_out_shape = [pool_out_shape[0] - self.kernel_size + self.stride, pool_out_shape[1] - self.kernel_size + self.stride]
+        cnn_out_size = cnn2_out_shape[0] * cnn2_out_shape[1] * self.out_channel
+        pool = nn.AvgPool2d(kernel_size=2)
+        self.cnn = nn.Sequential(
+            init_(nn.Conv2d(in_channels=input_channel,
+                            out_channels=self.out_channel,
+                            kernel_size=self.kernel_size,
+                            stride=self.stride)
+                  ),
+            pool,
+            init_(nn.Conv2d(in_channels=self.out_channel,
+                            out_channels=self.out_channel,
+                            kernel_size=self.kernel_size,
+                            stride=self.stride)),
+            active_func,
+            Flatten(),
+            init_(nn.Linear(cnn_out_size,
+                            self.hidden_size)),
+                            active_func,
+                            )
+    def forward(self, input):
+        output = self.cnn(input)
+        return output
+
 class CNNLayer(nn.Module):
     
     out_channel = 32
@@ -106,6 +153,46 @@ class CNNCategoricalActor(nn.Module):
         """
         logits = self.logits_net(obs).view(-1)
         return torch.argmax(logits)
+
+class CNNCategoricalActor2(nn.Module):
+    
+    def __init__(self, input_shape, act_dim, activation):
+        super().__init__()
+        self.input_shape = input_shape
+        self.act_dim = act_dim 
+        self.cnn_layer = CNNLayer2(input_shape)
+        self.linear_layer = mlp([64]+[256]+[act_dim], activation)
+        self.logits_net = nn.Sequential(self.cnn_layer, self.linear_layer)
+        
+    def distribution(self, obs):
+        logits = self.logits_net(obs)
+        return Categorical(logits=logits)
+
+    def log_prob_from_distribution(self, pi, act):
+        return pi.log_prob(act)
+
+    def forward(self, obs, act=None):
+        # Produce action distributions for given observations, and 
+        # optionally compute the log likelihood of given actions under
+        # those distributions.
+        pi = self.distribution(obs)
+        logp_a = None
+        if act is not None:
+            logp_a = self.log_prob_from_distribution(pi, act.view(-1))
+        return pi, logp_a
+
+    def save_model(self, pth):
+        torch.save(self.state_dict(), pth, _use_new_zipfile_serialization=False)
+
+    def load_model(self, pth):
+        self.load_state_dict(torch.load(pth))
+
+    def eval(self, obs):
+        """
+        return the best action
+        """
+        logits = self.logits_net(obs).view(-1)
+        return torch.argmax(logits)
 ####
 class RLAgent:
 
@@ -128,12 +215,34 @@ class RLAgent:
     def save_model(self, pth):
 
         self.actor.save_model(pth)
+    
+class RLAgent2:
+    
+    def __init__(self, state_shape, action_shape):
+        
+        self.actor = CNNCategoricalActor2(state_shape, action_shape, nn.ReLU)
+
+    def choose_action(self, obs):
+
+        state = torch.from_numpy(obs).float().unsqueeze(0).unsqueeze(0)
+        pi, _ = self.actor(state)
+        a_raw = pi.sample()
+
+        return a_raw
+
+    def load_model(self, pth):
+
+        self.actor.load_model(pth)
+    
+    def save_model(self, pth):
+
+        self.actor.save_model(pth)
 
 
 state_shape = [1, 25, 25]
 action_shape = 35
-load_pth = os.path.dirname(os.path.abspath(__file__)) + "/actor_4649.pth"
-agent = RLAgent(state_shape, action_shape)
+load_pth = os.path.dirname(os.path.abspath(__file__)) + "/actor_950_1.pth"
+agent = RLAgent2(state_shape, action_shape)
 agent.load_model(load_pth)
 # agent.save_model(load_pth)
 # load_path2 = os.path.dirname(os.path.abspath(__file__)) + "/actor_700.pth"
